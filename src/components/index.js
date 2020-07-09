a16b1de6fc076587d18398b0b1a0ee4067ab85fb
xiaobaihena <2507246051@qq.com>
2020-07-09 14:53:39 +0800

    点赞和打赏功能

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import Message from './Message'
+import Modal from './Modal'
 
-Vue.component('Message', Message)
+const components = {
+    Message,
+    Modal
+  }
+  
+  for (const [key, value] of Object.entries(components)) {
+    Vue.component(key, value)
+  }
+  
