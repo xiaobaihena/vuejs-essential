9d1c7cf6f8380ce46922768a45d88254d5993bf8
xiaobaihena <2507246051@qq.com>
2020-07-10 15:51:53 +0800

    社区侧栏：轮播图

--- src/components/index.js
+++ src/components/index.js
@@ -5,11 +5,16 @@ import Modal from './Modal'
 // 引入 Pagination.vue 的默认值
 import Pagination from './Pagination'
 
+// 引入 Slider.vue 的默认值
+import Slider from './Slider'
+
 const components = {
     Message,
     Modal,
      // 添加 Pagination 以便在循环中进行注册
-    Pagination
+    Pagination,
+    // 添加到对象以便在循环中进行注册
+    Slider
   }
   
   for (const [key, value] of Object.entries(components)) {
