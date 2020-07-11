527c026a690293787c1eb4875c6de981d3c5692d
xiaobaihena <2507246051@qq.com>
2020-07-11 09:00:29 +0800

    搜索结果排列

--- src/store/getters.js
+++ src/store/getters.js
@@ -149,33 +149,49 @@
         return filteredArticles
     }
     // 根据关键字 keyword 返回搜索结果
-export const getArticlesByKeyword = (state, getters) => (keyword) => {
-    let articles = getters.computedArticles
-    // 搜索结果
-    let results = []
-  
-    if (Array.isArray(articles)) {
-      articles.forEach((article) => {
-        let { articleId, title, content } = article
-        console.log(article);
-        // 该正则表示文章标题或内容中的关键字
-        const regex = new RegExp(`(${keyword})`, 'gi')
-       console.log(`keyword:${keyword}`);
-       if((title!==null)&&content!==null){
-        if (title.indexOf(keyword) !== -1 || content.indexOf(keyword) !== -1) {
-            // 使用 state.origin 代替 location.origin
-           const url = `${state.origin}/articles/${articleId}/content`
-            // 给文章标题中的关键字加上高亮，$1 匹配第一个括号的内容
-            title = title.replace(regex, '<span class="highlight">$1</span>')
-            // 给文章内容中的关键字加上高亮，只取内容前 100 个字
-            content = content.substr(0, 100).replace(regex, '<span class="highlight">$1</span>')
-            // 等价于 results.push(Object.assign({}, article, { url: url, title: title, content: content })) 
-            results.push({...article, ...{ url, title, content }})
-          }
-       }
-        
-      })
+    export const getArticlesByKeyword = (state, getters) => (keyword, filter) => {
+        let articles = getters.computedArticles
+        // 搜索结果
+        let results = []
+    
+        if (Array.isArray(articles)) {
+            articles.forEach((article) => {
+                let { articleId, title, content } = article
+                console.log(article);
+                // 该正则表示文章标题或内容中的关键字
+                const regex = new RegExp(`(${keyword})`, 'gi')
+                console.log(`keyword:${keyword}`);
+                if((title!==null)&&content!==null){
+                    if (title.indexOf(keyword) !== -1 || content.indexOf(keyword) !== -1) {
+                        // 使用 state.origin 代替 location.origin
+                    const url = `${state.origin}/articles/${articleId}/content`
+                        // 给文章标题中的关键字加上高亮，$1 匹配第一个括号的内容
+                        title = title.replace(regex, '<span class="highlight">$1</span>')
+                        // 给文章内容中的关键字加上高亮，只取内容前 100 个字
+                        content = content.substr(0, 100).replace(regex, '<span class="highlight">$1</span>')
+                        // 等价于 results.push(Object.assign({}, article, { url: url, title: title, content: content })) 
+                        results.push({...article, ...{ url, title, content }})
+                    }
+                }
+                
+            })
+        }
+         // 评估排序方式
+        switch (filter) {
+            case 'vote':
+            // 将赞的最多的文章排在前面
+            results.sort((a, b) => {
+                const alikeUsers = Array.isArray(a.likeUsers) ? a.likeUsers : []
+                const blikeUsers = Array.isArray(b.likeUsers) ? b.likeUsers : []
+
+                return blikeUsers.length - alikeUsers.length
+            })
+
+            break
+            default:
+            // 默认将标题中含有关键字的文章排在前面
+            results.sort((a, b) => a.title.indexOf(keyword) < b.title.indexOf(keyword))
+        }
+    
+       return results
     }
-  
-    return results
-  }
